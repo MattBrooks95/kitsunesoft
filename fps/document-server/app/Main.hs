00fdef3e9aa62c96bff9c8bf0f4eae8eff07b89f
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-08-27 13:02:00 +0900

    serving dummy data from haskell with yesod

--- fps/document-server/app/Main.hs
+++ fps/document-server/app/Main.hs
@@ -2,6 +2,14 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
+
+import qualified Data.Text as T (
+    Text
+    , concat
+    )
+
+import qualified Data.Map as M
 
 import Yesod
 
@@ -9,9 +17,26 @@ data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/sheet/#T.Text SheetR GET
+/sheets SheetsR GET
 |]
 
 instance Yesod HelloWorld
+
+dummySheets :: M.Map T.Text T.Text
+dummySheets = M.fromList [
+    ("sheet1", "a,b,c,d,e,f")
+    , ("sheet2", "g,h,i,j,k,l,m,n")
+    , ("sheet3", "o,p,q,r,s,t,u,v,w,x,y,z")
+    ]
+
+getSheetsR :: Handler Value
+getSheetsR = pure (toJSON (M.keys dummySheets))
+
+getSheetR :: T.Text -> Handler T.Text
+getSheetR sheetName = case M.lookup sheetName dummySheets of
+    Nothing -> pure $ T.concat ["couldn't find sheet with name", sheetName]
+    Just sheetContents -> pure sheetContents
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet|Hello World!|]
